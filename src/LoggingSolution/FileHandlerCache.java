43abcef03c7da73216ba76653e9a37777b3716c7
Anshul Agrawal <anshul.agraw999@gmail.com>
2022-06-06 21:29:53 +0530

    File logging

---
+++ src/LoggingSolution/FileHandlerCache.java
@@ -0,0 +1,28 @@
+package LoggingSolution;
+
+/**
+ * https://stackoverflow.com/questions/14842457/how-to-cache-file-handles
+ */
+
+import java.security.Key;
+
+/*public class FileHandlerCache {
+    LoadingCache<Key, Handle> graphs = CacheBuilder.newBuilder()
+            .maximumSize(100) // sensible value for open handles?
+            .expireAfterAccess(5, TimeUnit.MINUTES)
+            .removalListener(removalListener)
+            .build(
+                    new CacheLoader<Key, Handle>() {
+                        public Handle load(Key key) throws AnyException {
+                            return openHandle(key);
+                        }
+                    });
+
+    RemovalListener<Key, Handle> removalListener =
+            new RemovalListener<Key, Handle>() {
+                public void onRemoval(RemovalNotification<Key, Handle> removal) {
+                    Handle h = removal.getValue();
+                    h.close(); // tear down properly
+                }
+            };
+}*/
