4e8e5f74ce24bc0c1a553418b7dd57c6df68b5a6
Anshul Agrawal <anshul.agraw999@gmail.com>
2022-06-07 00:06:23 +0530

    Java semaphore example

---
+++ src/multiThreading/MutexVsSemaphore/SequenceGeneratorUsingSynchronizedMethod.java
@@ -0,0 +1,13 @@
+package multiThreading.MutexVsSemaphore;
+
+/**
+ * https://www.baeldung.com/java-mutex
+ */
+public class SequenceGeneratorUsingSynchronizedMethod extends SequenceGenerator {
+
+    @Override
+    public synchronized int getNextSequence() {
+        return super.getNextSequence();
+    }
+
+}
